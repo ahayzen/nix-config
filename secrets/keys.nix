8c3a1afdf5c2e53f274be3cd54949c60fb9a011a
Andrew Hayzen <ahayzen@gmail.com>
2024-03-01 17:51:28 +0000

    secrets: add xps-13-9360 to hosts

--- secrets/keys.nix
+++ secrets/keys.nix
@@ -15,6 +15,10 @@ let
   host__thinkpad-t480 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFyQGokh7SHDi+mSwd3MgMAH0I0IF/BxG/tRiCYxKgPF";
   user__thinkpad-t480__andrew-kdab = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM7mlMIrnPEXlFDYBp8Eflpl9E0nDu03LiVQrtXbNLFS andrew@ThinkPad-T480";
 
+  # XPS 13 9360
+  host__xps-13-9360 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1+V0m26Ni1WjixUmmYlSV7oE/zCvz/CHaafk3CmL+L";
+  user__xps-13-9360__andrew = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCq5KOXuuRBAPchTzslcD+/Lu89PUfsSxo6BiS90R0Svm71E5daDug+AaARgi6sUT0MaMzRsTCo885poqV9U+clY5C8sYBC+DbfZMS2LvqM8htXL8dbud3RzlBexcj27+8tVIvQntxU56Ok1l0T32nxMAnpMiCIsjsRyOOCjqJk0Flo2AdWP57So8j9j4N1PSfCGigzbpkuwqNwRsu64gPEaY/X0gqE4QkHzlydqhmMyDx9EXyhMrfE0g6ljeSURYjqXCB0pF0C8+MqcvkOOCTwrgL851ydBa71s43XxN4NLPQPQ4Cr+JFJE7oHtabg9QFg92vQqth0fAgnarsxijahfdTcIOgKkarCQrq7UPDkFxzr7o1ZrUPl6j8ckuMW3xem+3MeGynbkeJCljstaDOm96f1sQvI/n2aIwXqAIjCcZbmRCchiYpXicynDKfZOswqCmHozuny6/1azPNcEE6r0YXwyrdz38C4ZYjtcke5blZNRaUTJ/SkchKPGIMawGnPqYPssoElbKztHRfbPwJ9Cq8HU1XeqKd/PvIVFIYTnL8u84Hci3GWSvueEmzYG+QD60aI0jqsOG3BaV5/xIqAjZz+55tRvaPRu3Mcv/KWtDxaDTX0uJXawXqvaSer/d63jJX6w8oHTro8hM6IIi7avWIA8Wbja3+ikaZWNt7AjQ== andrew@andrew-XPS-13-9360";
+
   #
   # NAS
   #
@@ -34,19 +38,19 @@ in
   # Hosts that require access to desktop secrets
   #
   # This is for automatic upgrades
-  hosts-secrets-desktop = [ host__thinkpad-t480 ];
+  hosts-secrets-desktop = [ host__thinkpad-t480 host__xps-13-9360 ];
 
   # Users that require access to desktop secrets
   #
   # This is for developers to manage secrets or manually deploy flakes
-  users-secrets-desktop = [ user__thinkpad-t480__andrew-kdab ];
+  users-secrets-desktop = [ user__thinkpad-t480__andrew-kdab user__xps-13-9360__andrew ];
 
   #
   # Access settings for OpenSSH authorizedKeys
   #
 
   # Users that so might need access to headless hosts
-  users-openssh-headless-authorized = [ user__thinkpad-t480__andrew-kdab ];
+  users-openssh-headless-authorized = [ user__thinkpad-t480__andrew-kdab user__xps-13-9360__andrew ];
 
   # Hosts that are authorized to access headless hosts (eg for backup)
   hosts-openssh-headless-authorized = [ ];
