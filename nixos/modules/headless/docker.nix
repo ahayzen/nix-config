acb2dc81a6d5c867fcdc8c37b76479e3e4e52d77
Andrew Hayzen <ahayzen@gmail.com>
2024-03-12 21:40:22 +0000

    headless: allow rootless processes to bind to privileged ports

--- nixos/modules/headless/docker.nix
+++ nixos/modules/headless/docker.nix
@@ -58,6 +58,14 @@
       };
     };
 
+    # Allow for rootless docker to bind to privileged ports
+    # our lowest is http at 80
+    #
+    # TODO: can we limit this to just the docker process
+    boot.kernel.sysctl = {
+      "net.ipv4.ip_unprivileged_port_start" = 80;
+    };
+
     virtualisation.docker = {
       enable = true;
 
