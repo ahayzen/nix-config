a628eade26c135105891b5c544048df03af64dcf
Andrew Hayzen <ahayzen@gmail.com>
2024-03-26 21:16:13 +0000

    modules/all: use systemd in stage 1 rather than script

--- nixos/modules/all/boot.nix
+++ nixos/modules/all/boot.nix
@@ -6,21 +6,28 @@
   # TODO: FUTURE: 24.05: investigate `boot.loader.systemd-boot.bootCounting`
   # for automatic boot assessment with systemd-boot
   # https://github.com/NixOS/nixpkgs/pull/284135/
-  boot.loader = {
-    efi = {
-      # Allow for reordering the boot entries
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi";
-    };
+  boot = {
+    # Enable systemd for stage 1
+    #
+    # TODO: FUTURE: this might become default
+    initrd.systemd.enable = true;
 
-    # Enable systemd boot
-    systemd-boot = {
-      enable = lib.mkDefault true;
-      configurationLimit = 10;
-      memtest86.enable = !config.ahayzen.headless;
-    };
+    loader = {
+      efi = {
+        # Allow for reordering the boot entries
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
 
-    # When headless have a reduced timeout to decrease boot times
-    timeout = if config.ahayzen.headless then 1 else 5;
+      # Enable systemd boot
+      systemd-boot = {
+        enable = lib.mkDefault true;
+        configurationLimit = 10;
+        memtest86.enable = !config.ahayzen.headless;
+      };
+
+      # When headless have a reduced timeout to decrease boot times
+      timeout = if config.ahayzen.headless then 1 else 5;
+    };
   };
 }
