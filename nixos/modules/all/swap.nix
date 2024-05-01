a89469bbb28251eb17d57faee82a5fb82686c454
Andrew Hayzen <ahayzen@gmail.com>
2024-05-01 23:06:04 +0100

    swap: use 2GB swap file everywhere for now

--- nixos/modules/all/swap.nix
+++ nixos/modules/all/swap.nix
@@ -2,12 +2,12 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-{ config, ... }: {
-  # Create a swapfile of 1GB for headless and 2GB for desktop
+{
+  # Create a swapfile of 2GB
   swapDevices = [
     {
       device = "/var/lib/swapfile";
-      size = if config.ahayzen.headless then 1024 else 2 * 1024;
+      size = 2 * 1024;
     }
   ];
 
