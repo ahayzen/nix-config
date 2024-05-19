ddc29f99baf2d754a945f10561b51293e5c145af
Andrew Hayzen <ahayzen@gmail.com>
2024-05-19 18:04:48 +0100

    vps: tweak permissions so others cannot read

--- nixos/hosts/vps/default.nix
+++ nixos/hosts/vps/default.nix
@@ -45,14 +45,14 @@
     local-py_ahayzen-com = {
       file = ../../../secrets/local-py_ahayzen-com.age;
       # Set correct owner otherwise docker cannot read the file
-      mode = "0444";
+      mode = "0600";
       owner = "unpriv";
       group = "unpriv";
     };
     local-py_yumekasaito-com = {
       file = ../../../secrets/local-py_yumekasaito-com.age;
       # Set correct owner otherwise docker cannot read the file
-      mode = "0444";
+      mode = "0600";
       owner = "unpriv";
       group = "unpriv";
     };
