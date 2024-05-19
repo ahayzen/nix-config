574aaa785ea84c679926284d48b0eb01e8d7c745
Andrew Hayzen <ahayzen@gmail.com>
2024-05-19 10:24:53 +0100

    vps: VLE has EFI boot rather than BIOS

--- nixos/modules/all/boot.nix
+++ nixos/modules/all/boot.nix
@@ -16,7 +16,6 @@
       efi = {
         # Allow for reordering the boot entries
         canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot/efi";
       };
 
       # Enable systemd boot
