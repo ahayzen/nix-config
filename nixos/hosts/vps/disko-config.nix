05d43866a414e732b4c4c2069093ef8fa7f9055c
Andrew Hayzen <ahayzen@gmail.com>
2024-05-19 10:24:53 +0100

    vps: VLE has EFI boot rather than BIOS

--- nixos/hosts/vps/disko-config.nix
+++ nixos/hosts/vps/disko-config.nix
@@ -11,10 +11,6 @@
         content = {
           type = "gpt";
           partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-            };
             ESP = {
               size = "512M";
               type = "EF00";
