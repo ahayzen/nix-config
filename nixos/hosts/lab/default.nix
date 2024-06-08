cc6f70cd4aef1369ca9e55170a395a949c647866
Andrew Hayzen <ahayzen@gmail.com>
2024-06-08 22:11:50 +0100

    lab: add sshfs mount to nas
    Related to #142

--- nixos/hosts/lab/default.nix
+++ nixos/hosts/lab/default.nix
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-{ config, inputs, lib, options, ... }:
+{ config, inputs, lib, options, pkgs, ... }:
 {
   imports = [
     ./disko-config.nix
@@ -75,6 +75,86 @@
 
   # Enable thermal control as this is an intel laptop
   services.thermald.enable = true;
+
+  # Use SSHFS to mount folders from NAS for data and restic backups
+  system.fsPackages = [ pkgs.sshfs ];
+  fileSystems = lib.mkIf (!config.ahayzen.testing) {
+    mntbackuprestic = {
+      # TODO: can we use name?
+      # https://github.com/ahayzen/nix-config/issues/141
+      device = "restic@192.168.1.196:/restic/repository";
+      mountPoint = "/mnt/backup-restic";
+      fsType = "sshfs";
+      options =
+        [
+          "_netdev"
+          "x-systemd.automount"
+
+          "allow_other"
+
+          "IdentityFile=/etc/ssh/ssh_host_ed25519_key"
+          "Port=8022"
+
+          "reconnect"
+          "ServerAliveInterval=15"
+
+          "follow_symlinks"
+
+          "idmap=user"
+          "uid=2000"
+          "gid=2000"
+        ];
+    };
+    mntdata = {
+      # TODO: can we use name?
+      # https://github.com/ahayzen/nix-config/issues/141
+      device = "restic@192.168.1.196:/restic/data";
+      mountPoint = "/mnt/data";
+      fsType = "sshfs";
+      options =
+        [
+          "_netdev"
+          "x-systemd.automount"
+
+          # might need user_allow_other in fuse cofig?
+          "allow_other"
+
+          "IdentityFile=/etc/ssh/ssh_host_ed25519_key"
+          "Port=8022"
+
+          "reconnect"
+          "ServerAliveInterval=15"
+
+          "follow_symlinks"
+
+          "idmap=user"
+          "uid=2000"
+          "gid=2000"
+        ];
+    };
+  };
+
+  # Emulate sshfs mount folders for testing
+  systemd.tmpfiles.settings = lib.mkIf (config.ahayzen.testing) {
+    "99-sshfs-mount" = {
+      "/mnt/backup-restic" = {
+        d = {
+          age = "-";
+          group = "unpriv";
+          mode = "0750";
+          user = "unpriv";
+        };
+      };
+      "/mnt/data" = {
+        d = {
+          age = "-";
+          group = "unpriv";
+          mode = "0750";
+          user = "unpriv";
+        };
+      };
+    };
+  };
 
   # Increase disk size for build VM
   virtualisation.vmVariant.virtualisation.diskSize = 2 * 1024;
