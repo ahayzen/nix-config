a4dccc1d137038ee3eef851a8d50454de3985cdb
Andrew Hayzen <ahayzen@gmail.com>
2024-06-09 19:49:32 +0100

    github: split renovate configs
    Related to #125

---
+++ .github/workflows/renovate-self-host.js
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: Andrew Hayzen <ahayzen@gmail.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
+module.exports = {
+    branchPrefix: "update-renovate/",
+    gitAuthor: "Renovate Bot <bot@renovateapp.com>",
+    onboarding: false,
+    platform: "github",
+    forkProcessing: "disabled",
+    repositories: [
+        "ahayzen/nix-config",
+    ],
+}
