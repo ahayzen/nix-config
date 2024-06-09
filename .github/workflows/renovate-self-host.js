093baed8e407f2d2c5fc0b5bd4fad15dfe00aba5
Andrew Hayzen <ahayzen@gmail.com>
2024-06-09 22:43:58 +0100

    github: use self-host renovate file for action

--- .github/workflows/renovate-self-host.js
+++ .github/workflows/renovate-self-host.js
@@ -3,11 +3,8 @@
 // SPDX-License-Identifier: MPL-2.0
 
 module.exports = {
-    branchPrefix: "update-renovate/",
-    gitAuthor: "Renovate Bot <bot@renovateapp.com>",
     onboarding: false,
     platform: "github",
-    forkProcessing: "disabled",
     repositories: [
         "ahayzen/nix-config",
     ],
