376ad4f3ae517dc1d11c1c2d103c28485f83f1fd
Andrew Hayzen <ahayzen@gmail.com>
2024-06-22 21:28:58 +0100

    lab: move sshfs config into it's own nix file
    Related to #142

--- nixos/hosts/lab/default.nix
+++ nixos/hosts/lab/default.nix
@@ -9,6 +9,7 @@
     inputs.nixos-hardware.nixosModules.common-cpu-intel-sandy-bridge
     ./hardware.nix
     ./periodic.nix
+    ./sshfs.nix
   ];
 
   # System76 Pangolin Performance uses BIOS so we need to disable systemd-boot and use grub
@@ -76,86 +77,6 @@
 
   # Enable thermal control as this is an intel laptop
   services.thermald.enable = true;
-
-  # Use SSHFS to mount folders from NAS for data and restic backups
-  system.fsPackages = [ pkgs.sshfs ];
-  fileSystems = lib.mkIf (!config.ahayzen.testing) {
-    mntbackuprestic = {
-      # TODO: can we use name?
-      # https://github.com/ahayzen/nix-config/issues/141
-      device = "restic@192.168.1.196:/restic/repository";
-      mountPoint = "/mnt/backup-restic";
-      fsType = "sshfs";
-      options =
-        [
-          "_netdev"
-
-          "allow_other"
-
-          "IdentityFile=/etc/ssh/ssh_host_ed25519_key"
-          "Port=8022"
-
-          "reconnect"
-          "ServerAliveInterval=10"
-          "ServerAliveCountMax=3"
-
-          "follow_symlinks"
-
-          "idmap=user"
-          "uid=2000"
-          "gid=2000"
-        ];
-    };
-    mntdata = {
-      # TODO: can we use name?
-      # https://github.com/ahayzen/nix-config/issues/141
-      device = "restic@192.168.1.196:/restic/data";
-      mountPoint = "/mnt/data";
-      fsType = "sshfs";
-      options =
-        [
-          "_netdev"
-
-          # might need user_allow_other in fuse cofig?
-          "allow_other"
-
-          "IdentityFile=/etc/ssh/ssh_host_ed25519_key"
-          "Port=8022"
-
-          "reconnect"
-          "ServerAliveInterval=10"
-          "ServerAliveCountMax=3"
-
-          "follow_symlinks"
-
-          "idmap=user"
-          "uid=2000"
-          "gid=2000"
-        ];
-    };
-  };
-
-  # Emulate sshfs mount folders for testing
-  systemd.tmpfiles.settings = lib.mkIf (config.ahayzen.testing) {
-    "99-sshfs-mount" = {
-      "/mnt/backup-restic" = {
-        d = {
-          age = "-";
-          group = "unpriv";
-          mode = "0750";
-          user = "unpriv";
-        };
-      };
-      "/mnt/data" = {
-        d = {
-          age = "-";
-          group = "unpriv";
-          mode = "0750";
-          user = "unpriv";
-        };
-      };
-    };
-  };
 
   # Increase disk size for build VM
   virtualisation.vmVariant.virtualisation.diskSize = 2 * 1024;
