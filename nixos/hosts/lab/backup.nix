9bc17b63de5f8e6a65845c1c82a82ad7d085d434
Andrew Hayzen <ahayzen@gmail.com>
2024-06-30 10:17:17 +0100

    backup: rename backup-dir so we don't have backup twice in the path

--- nixos/hosts/lab/backup.nix
+++ nixos/hosts/lab/backup.nix
@@ -7,12 +7,12 @@
   systemd = {
     services."backup-machines" = {
       script = ''
-        /run/wrappers/bin/sudo --user=unpriv ${pkgs.coreutils}/bin/mkdir -p /mnt/data/backup/lab/backup/docker-compose-runner
+        /run/wrappers/bin/sudo --user=unpriv ${pkgs.coreutils}/bin/mkdir -p /mnt/data/backup/lab/outdated/docker-compose-runner
         /run/wrappers/bin/sudo --user=unpriv ${pkgs.coreutils}/bin/mkdir -p /mnt/data/backup/lab/latest/docker-compose-runner
-        /run/wrappers/bin/sudo ${pkgs.rsync}/bin/rsync --archive --backup-dir=/mnt/data/backup/lab/backup/docker-compose-runner --checksum --delete --human-readable --ignore-times --numeric-ids --partial --progress /var/lib/docker-compose-runner/ /mnt/data/backup/lab/latest/docker-compose-runner
-        /run/wrappers/bin/sudo --user=unpriv ${pkgs.coreutils}/bin/mkdir -p /mnt/data/backup/vps/backup/docker-compose-runner
+        /run/wrappers/bin/sudo ${pkgs.rsync}/bin/rsync --archive --backup-dir=/mnt/data/backup/lab/outdated/docker-compose-runner --checksum --delete --human-readable --ignore-times --numeric-ids --partial --progress /var/lib/docker-compose-runner/ /mnt/data/backup/lab/latest/docker-compose-runner
+        /run/wrappers/bin/sudo --user=unpriv ${pkgs.coreutils}/bin/mkdir -p /mnt/data/backup/vps/outdated/docker-compose-runner
         /run/wrappers/bin/sudo --user=unpriv ${pkgs.coreutils}/bin/mkdir -p /mnt/data/backup/vps/latest/docker-compose-runner
-        /run/wrappers/bin/sudo ${pkgs.rsync}/bin/rsync --archive --backup-dir=/mnt/data/backup/vps/backup/docker-compose-runner --checksum --delete --human-readable --ignore-times --numeric-ids --partial --progress --rsh="${pkgs.openssh}/bin/ssh -i /etc/ssh/ssh_host_ed25519_key -p 8022" --rsync-path="sudo rsync" headless@ahayzen.com:/var/lib/docker-compose-runner/ /mnt/data/backup/vps/latest/docker-compose-runner
+        /run/wrappers/bin/sudo ${pkgs.rsync}/bin/rsync --archive --backup-dir=/mnt/data/backup/vps/outdated/docker-compose-runner --checksum --delete --human-readable --ignore-times --numeric-ids --partial --progress --rsh="${pkgs.openssh}/bin/ssh -i /etc/ssh/ssh_host_ed25519_key -p 8022" --rsync-path="sudo rsync" headless@ahayzen.com:/var/lib/docker-compose-runner/ /mnt/data/backup/vps/latest/docker-compose-runner
       '';
       serviceConfig = {
         Type = "oneshot";
