2df8d2fd2e446f4bd137b0a70e995320d6ffd8ff
Andrew Hayzen <ahayzen@gmail.com>
2024-07-04 22:10:26 +0100

    vps: split wagtail-yumekasaito into a separate optional service

---
+++ nixos/hosts/vps/wagtail-yumekasaito/default.nix
@@ -0,0 +1,45 @@
+# SPDX-FileCopyrightText: Andrew Hayzen <ahayzen@gmail.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+{ config, options, lib, ... }:
+{
+  options.ahayzen.vps.wagtail-yumekasaito = lib.mkOption {
+    default = true;
+    type = lib.types.bool;
+  };
+
+  config = lib.mkIf (config.ahayzen.vps.wagtail-yumekasaito) {
+    age.secrets = lib.mkIf (!config.ahayzen.testing) {
+      local-py_yumekasaito-com = {
+        file = ../../../../secrets/local-py_yumekasaito-com.age;
+        # Set correct owner otherwise docker cannot read the file
+        mode = "0600";
+        owner = "unpriv";
+        group = "unpriv";
+      };
+    };
+
+    ahayzen.docker-compose-files = [ ./compose.wagtail-yumekasaito.yml ];
+
+    environment.etc = {
+      "caddy/sites/yumekasaito.Caddyfile".source =
+        if config.ahayzen.testing
+        then ./yumekasaito.Caddyfile.vm
+        else ./yumekasaito.Caddyfile;
+      "yumekasaito.com/local.1.py".
+      source =
+        if config.ahayzen.testing
+        then ./local.vm.py
+        else config.age.secrets.local-py_yumekasaito-com.path;
+    };
+
+    # Reload if static files change
+    #
+    # Note agenix files are not possible and will need the version bumping
+    # which causes the hash of the docker-compose file to change.
+    systemd.services."docker-compose-runner".reloadTriggers = [
+      (builtins.hashFile "sha256" config.environment.etc."caddy/sites/yumekasaito.Caddyfile".source)
+    ];
+  };
+}
