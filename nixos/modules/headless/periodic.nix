ffe9528de53a158cf16fb825110d5169ecb197c4
Andrew Hayzen <ahayzen@gmail.com>
2024-07-23 22:11:52 +0100

    headless: add periodic-weekly option to periodic

--- nixos/modules/headless/periodic.nix
+++ nixos/modules/headless/periodic.nix
@@ -4,34 +4,69 @@
 
 { config, lib, pkgs, ... }:
 {
-  options.ahayzen.periodic-daily-commands = lib.mkOption {
-    default = [ ];
-    type = lib.types.listOf lib.types.str;
+  options.ahayzen = {
+    periodic-daily-commands = lib.mkOption {
+      default = [ ];
+      type = lib.types.listOf lib.types.str;
+    };
+
+    periodic-weekly-commands = lib.mkOption {
+      default = [ ];
+      type = lib.types.listOf lib.types.str;
+    };
   };
 
   config = {
     systemd = {
-      services."periodic-daily" = {
-        serviceConfig = {
-          Type = "oneshot";
+      services = {
+        "periodic-daily" = {
+          serviceConfig = {
+            Type = "oneshot";
+          };
+          script = builtins.foldl'
+            (commands: command: ''
+              ${commands}
+              ${command}
+            '') ""
+            config.ahayzen.periodic-daily-commands;
         };
-        script = builtins.foldl'
-          (commands: command: ''
-            ${commands}
-            ${command}
-          '') ""
-          config.ahayzen.periodic-daily-commands;
+
+        "periodic-weekly" = {
+          serviceConfig = {
+            Type = "oneshot";
+          };
+          script = builtins.foldl'
+            (commands: command: ''
+              ${commands}
+              ${command}
+            '') ""
+            config.ahayzen.periodic-weekly-commands;
+        };
       };
 
-      timers."periodic-daily" = {
-        # Only enable if there are commands
-        enable = config.ahayzen.periodic-daily-commands != [ ];
-        wantedBy = [ "timers.target" ];
-        timerConfig = {
-          OnCalendar = "daily";
-          Unit = "periodic-daily.service";
-          RandomizedDelaySec = "30m";
-          Persistent = true;
+      timers = {
+        "periodic-daily" = {
+          # Only enable if there are commands
+          enable = config.ahayzen.periodic-daily-commands != [ ];
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnCalendar = "daily";
+            Unit = "periodic-daily.service";
+            RandomizedDelaySec = "30m";
+            Persistent = true;
+          };
+        };
+
+        "periodic-weekly" = {
+          # Only enable if there are commands
+          enable = config.ahayzen.periodic-weekly-commands != [ ];
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnCalendar = "weekly";
+            Unit = "periodic-daily.service";
+            RandomizedDelaySec = "30m";
+            Persistent = true;
+          };
         };
       };
     };
