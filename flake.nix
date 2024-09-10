4f2321eae5963f09d64f861f4a23a75bf0d04140
Andrew Hayzen <ahayzen@gmail.com>
2024-09-10 21:42:57 +0100

    flake: use NixOS instead of nixos to see if it helps renovate

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs =
