70c1e9ff7afd0bedf5eb4b649eb35b3f1ea4e883
Andrew Hayzen <ahayzen@gmail.com>
2024-11-30 18:51:11 +0000

    desktop: update gnome install to only use core and have gsetting overrides

--- nixos/modules/desktop/gnome.nix
+++ nixos/modules/desktop/gnome.nix
@@ -3,11 +3,6 @@
 # SPDX-License-Identifier: MPL-2.0
 
 { pkgs, ... }: {
-  # Install a terminal as this is normally from gnome.core-utilities
-  environment.systemPackages = with pkgs; [
-    gnome-console
-  ];
-
   services = {
     # Do not install the apps for GNOME
     # https://nixos.org/manual/nixos/stable/#sec-gnome-without-the-apps
@@ -18,16 +13,57 @@
       enable = true;
 
       # Enable the GNOME Desktop Environment.
-      desktopManager.gnome.enable = true;
+      desktopManager.gnome = {
+        enable = true;
+
+        extraGSettingsOverrides = ''
+          # Enable week numbers in top bar calendar
+          [org.gnome.desktop.calendar]
+          show-weekdate = true
+
+          # Enable automatic timezones from geoip
+          [org.gnome.desktop.datetime]
+          automatic-timezone = true
+
+          # Touchpad tweaks
+          [org.gnome.desktop.peripherals.touchpad]
+          disable-while-typing = false
+          tap-to-click = true
+
+
+          # Enable night light
+          [org.gnome.settings-daemon.plugins.color]
+          night-light-enabled = true
+
+          # Do not suspend when on AC after a period of time
+          [org.gnome.settings-daemon.plugins.power]
+          sleep-inactive-ac-type = 'nothing'
+
+
+          # Fix alphabetical ordering of app picker and disable all extensions
+          [org.gnome.shell]
+          app-picker-layout = []
+          enabled-extensions = []
+
+
+          # Enable location services for night light and other apps
+          [org.gnome.system.location]
+          enabled = true
+        '';
+
+        extraGSettingsOverridePackages = [
+          # org.gnome.desktop
+          pkgs.gsettings-desktop-schemas
+          # org.gnome.shell
+          pkgs.gnome.gnome-shell
+        ];
+      };
       displayManager.gdm.enable = true;
 
       # We do not want xterm enabled
       excludePackages = [ pkgs.xterm ];
       desktopManager.xterm.enable = false;
     };
-
-    # TODO: integrate gsettings overrides
-    # https://nixos.org/manual/nixos/stable/#sec-gnome-gsettings-overrides
   };
 
   # TODO: install the gnome apps
