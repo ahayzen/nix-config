a73c2d905b4d50f445b964ffdeb26d74de7d4a4c
Andrew Hayzen <ahayzen@gmail.com>
2024-12-07 12:25:52 +0000

    desktop: configure flatpak remote once network is available

--- nixos/modules/desktop/flatpak.nix
+++ nixos/modules/desktop/flatpak.nix
@@ -5,10 +5,12 @@
 { pkgs, ... }: {
   services.flatpak.enable = true;
   systemd.services.configure-flathub-repo = {
+    after = [ "network-online.target" ];
     wantedBy = [ "multi-user.target" ];
+    wants = [ "network-online.target" ];
     path = [ pkgs.flatpak ];
     script = ''
-      flatpak remote-add --if-not-exists flathub-system https://flathub.org/repo/flathub.flatpakrepo
+      flatpak remote-add --if-not-exists system-flathub https://flathub.org/repo/flathub.flatpakrepo
     '';
   };
 
