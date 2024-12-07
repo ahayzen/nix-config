716bf8320d763c198cf0cd7d5ae961696f8916d6
Andrew Hayzen <ahayzen@gmail.com>
2024-12-07 23:54:20 +0000

    desktop: override alacritty config to force a cursor theme

--- nixos/modules/desktop/developer/alacritty.nix
+++ nixos/modules/desktop/developer/alacritty.nix
@@ -2,9 +2,21 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  # Alacritty doesn't have a cursor theme set as there no global one in Nix
+  # https://github.com/NixOS/nixpkgs/issues/22652
+  # https://github.com/alacritty/alacritty/issues/6703
+  # So for now force a theme otherwise there is no mouse cursor
+  alacritty = pkgs.alacritty.overrideAttrs (finalAttrs: {
+    postInstall = (finalAttrs.postInstall or "") + ''
+      wrapProgram $out/bin/alacritty --set XCURSOR_THEME Adwaita
+    '';
+  });
+in
+{
   # Ensure that we always have a terminal installed
   environment.systemPackages = [
-    pkgs.alacritty
+    alacritty
   ];
 }
