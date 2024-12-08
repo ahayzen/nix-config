675f871e0d09e537c62763c8c1e403dbe6a00c41
Andrew Hayzen <ahayzen@gmail.com>
2024-12-08 11:45:40 +0000

    desktop: instal openssh and allow it in the firewall

--- nixos/modules/desktop/firewall.nix
+++ nixos/modules/desktop/firewall.nix
@@ -2,6 +2,14 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+{ pkgs, ... }:
 {
   # TODO: allow common port ranges for mDNS etc?
+
+  # GNOME can enable openssh to install and allow the port
+  environment.systemPackages = [
+    pkgs.openssh
+  ];
+
+  networking.firewall.allowedTCPPorts = [ 22 ];
 }
