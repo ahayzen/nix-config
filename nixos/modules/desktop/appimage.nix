56100bba4b34abfe90215c4b9099bb5785c3f729
Andrew Hayzen <ahayzen@gmail.com>
2024-12-08 10:55:13 +0000

    desktop: disable appimage binfmt as it prevents appimages in containers

--- nixos/modules/desktop/appimage.nix
+++ nixos/modules/desktop/appimage.nix
@@ -3,11 +3,6 @@
 # SPDX-License-Identifier: MPL-2.0
 
 {
-  programs.appimage = {
-    # Enable appimage-run
-    enable = true;
-
-    # Recognise appimage files and use appimage-run to launch them
-    binfmt = true;
-  };
+  # Install appimage-run
+  programs.appimage.enable = true;
 }
