7fde2b6842219b87a5bd53df95516efc33b90565
Andrew Hayzen <ahayzen@gmail.com>
2024-12-14 23:36:46 +0000

    home-manager: use starship prompt

--- home-manager/andrew/default.nix
+++ home-manager/andrew/default.nix
@@ -10,6 +10,7 @@
     ./git.nix
     ./gnome.nix
     ./helix.nix
+    ./starship.nix
     ./zellij.nix
   ];
 
@@ -26,27 +27,6 @@
 
     home = {
       homeDirectory = "/home/andrew";
-      sessionVariables = {
-        # Custom bash prompt adapted from
-        # https://bash-prompt-generator.org/
-        #
-        # user@host:/current/work/dir
-        # 0 (branch) $
-        #
-        # Use prompt command as helper
-        # - Store the exit code so it's not lost
-        # - Store the git branch name if there is one
-        PROMPT_COMMAND = ''PS1_CMD1=\$?; PS1_CMD2=\$(git branch --show-current 2>/dev/null)'';
-        # First line is [lime]user@hostname[/lime]:[aqua]/path[/aqua]
-        # Second line has optional components
-        # - [red]exit status[/red] (if not zero)
-        # - [yellow](branch)[/yellow] (if git branch)
-        # - prompt
-        #
-        # TODO: could use "..." instead of '' here with correct escaping?
-        # as this ends up as "..." in the generated variables
-        PS1 = ''\[\e[92;1m\]\u@\h\[\e[0m\]:\[\e[96;1m\]\w\n\[\e[91m\]\$([[ \$PS1_CMD1 == 0 ]] || echo \"\$PS1_CMD1 \")\[\e[0m\]\[\e[93;1m\]\$([[ \$PS1_CMD2 == \"\" ]] || echo \"(\"\$PS1_CMD2\") \")\[\e[0m\]\[\e[92;1m\]\\$\[\e[0m\] '';
-      };
       stateVersion = "24.05";
       username = "andrew";
     };
---
+++ home-manager/andrew/starship.nix
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: Andrew Hayzen <ahayzen@gmail.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+{
+  programs = {
+    starship = {
+      enable = true;
+
+      catppuccin.enable = true;
+    };
+  };
+}
