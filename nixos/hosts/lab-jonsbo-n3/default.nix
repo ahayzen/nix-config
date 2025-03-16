f01f5d561105edf751425df74eef7958ff84396c
Andrew Hayzen <ahayzen@gmail.com>
2025-03-16 21:16:03 +0000

    lab: use mkForce for disabling rathole

--- nixos/hosts/lab-jonsbo-n3/default.nix
+++ nixos/hosts/lab-jonsbo-n3/default.nix
@@ -20,7 +20,7 @@
   ahayzen = {
     docker-compose-files = [ ./compose.lab-jonsbo-n3.yml ];
     hostName = "lab-jonsbo-n3";
-    lab.rathole = false;
+    lab.rathole = lib.mkForce false;
   };
 
   # Seed host keys for places we SSH to
