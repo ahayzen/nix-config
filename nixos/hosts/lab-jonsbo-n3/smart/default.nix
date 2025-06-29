8d3a3c3ad5673a4326893e17eb772dfb9263a2a2
Andrew Hayzen <ahayzen@gmail.com>
2025-06-29 14:08:38 +0100

    smart: add a timer that triggers SMART runs weekly

---
+++ nixos/hosts/lab-jonsbo-n3/smart/default.nix
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: Andrew Hayzen <ahayzen@gmail.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+{ config, pkgs, ... }:
+{
+  systemd = {
+    services."smart-weekly" = {
+      serviceConfig = {
+        ExecStart =
+          let
+            python = pkgs.python3.withPackages (ps: with ps; [ pysmart ]);
+          in
+          "${python.interpreter} ${./smart_monitor.py}";
+        Type = "oneshot";
+      };
+    };
+
+    timers."smart-weekly" = {
+      # Enable when not in testing mode
+      enable = !config.ahayzen.testing;
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "Mon, 04:15";
+        Unit = "smart-weekly.service";
+        Persistent = true;
+      };
+    };
+  };
+}
---
+++ nixos/hosts/lab-jonsbo-n3/smart/smart_monitor.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+#
+# SPDX-FileCopyrightText: Andrew Hayzen <ahayzen@gmail.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from pySMART import Device
+from time import sleep
+
+ASSESSMENT_PASS = "PASS"
+GET_SELFTEST_RESULT_IN_PROGRESS = 1
+TEMPERATURE_MIN = 10
+TEMPERATURE_MAX = 50
+TEST_RESULT_PASS = "Completed without error"
+TEST_TYPE_SHORT = "short"
+TEST_TYPE_LONG = "long"
+
+
+def self_test_start(device: Device, test_type: str):
+    print(
+        "%s attempting to start self test: %s"
+        % (device.dev_reference, test_type)
+    )
+
+    # Ensure that there isn't a test running already
+    if device.get_selftest_result()[0] == GET_SELFTEST_RESULT_IN_PROGRESS:
+        print("%s self test in progress ignore" % device.dev_reference)
+        return 0
+
+    # Start a self test
+    (exit_code, test_result) = device.run_selftest(test_type)
+    if exit_code != 0:
+        print(
+            "%s self test failed to run and wait: %s"
+            % (device.dev_reference, exit_code)
+        )
+        return 1
+
+    return 0
+
+
+def self_test_wait(device: Device):
+    print("%s waiting for test result" % device.dev_reference)
+
+    # Wait for the test to complete
+    test_result = device.get_selftest_result()
+    while test_result[0] != GET_SELFTEST_RESULT_IN_PROGRESS:
+        test_result = device.get_selftest_result()
+        sleep(1)
+
+    # Check we have a successful test result
+    if test_result[0] != 0:
+        print(
+            "%s unknown test result state: %s"
+            % (device.dev_reference, test_result)
+        )
+        return 1
+
+    # Check the result
+    if test_result[1].status == TEST_RESULT_PASS:
+        print("%s self test passed" % device.dev_reference)
+        return 0
+    else:
+        print(
+            "%s self test failed with: %s"
+            % (device.dev_reference, test_result[1].status)
+        )
+        return 1
+
+
+if __name__ == "__main__":
+    from datetime import datetime
+    from pySMART import DeviceList
+
+    # Long test once per month and short test for other weeks
+    day_of_year = datetime.now().timetuple().tm_yday
+    week_of_year = day_of_year // 7
+    monthly_cycle = week_of_year % 4 == 0
+    test_type = TEST_TYPE_LONG if monthly_cycle % 4 == 0 else TEST_TYPE_SHORT
+
+    # Find the devices
+    devices = DeviceList()
+
+    # Attempt to run SMART on each device
+    for device in devices:
+        if self_test_start(device, test_type) != 0:
+            exit(1)
+
+    # Wait for completion
+    for device in devices:
+        if self_test_wait(device) != 0:
+            exit(1)
+
+    # Check assessment and temperature of each device
+    for device in devices:
+        if device.assessment != ASSESSMENT_PASS:
+            print(
+                "%s SMART assessment failed: %s"
+                % (device.dev_reference, device.assessment)
+            )
+            exit(1)
+
+        if (
+            device.temperature < TEMPERATURE_MIN
+            or device.temperature > TEMPERATURE_MAX
+        ):
+            print(
+                "%s temperature failed: %s"
+                % (device.dev_reference, device.temperature)
+            )
+            exit(1)
+
+    print("SMART monitor passed")
+    exit(0)
