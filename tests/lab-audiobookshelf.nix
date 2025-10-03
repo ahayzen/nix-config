adeaa6534fea90a9e18638c90001c07fca0a17e3
Andrew Hayzen <ahayzen@gmail.com>
2025-10-03 22:58:38 +0100

    lab: move audiobookshelf database to /var/cache

--- tests/lab-audiobookshelf.nix
+++ tests/lab-audiobookshelf.nix
@@ -225,7 +225,7 @@
       backup.succeed("mkdir -p /tmp/backup-root-lab")
 
       # Check that the permissions are correct
-      lab.succeed("ls -nd /var/lib/docker-compose-runner/audiobookshelf/config/absdatabase.sqlite | awk 'NR==1 {if ($3 == 2000) {exit 0} else {exit 1}}'")
+      lab.succeed("ls -nd /var/cache/docker-compose-runner/audiobookshelf/config/absdatabase.sqlite | awk 'NR==1 {if ($3 == 2000) {exit 0} else {exit 1}}'")
 
       # Trigger a snapshot
       labdayofweek = datetime.datetime.today().strftime('%w')
@@ -240,9 +240,6 @@
       # Check that known files exist and permissions are correct
       backup.succeed("test -e /tmp/backup-root-lab/docker-compose-runner/audiobookshelf/config/absdatabase-snapshot-" + labdayofweek + ".sqlite")
       backup.succeed("ls -nd /tmp/backup-root-lab/docker-compose-runner/audiobookshelf/config/absdatabase-snapshot-" + labdayofweek + ".sqlite | awk 'NR==1 {if ($3 == 2000) {exit 0} else {exit 1}}'")
-      backup.succeed("test -e /tmp/backup-root-lab/docker-compose-runner/audiobookshelf/config/absdatabase.sqlite")
-      backup.succeed("ls -nd /tmp/backup-root-lab/docker-compose-runner/audiobookshelf/config/absdatabase.sqlite | awk 'NR==1 {if ($3 == 2000) {exit 0} else {exit 1}}'")
-
 
     #
     # Test auto backup in lab
@@ -262,8 +259,6 @@
       # Check that known files exist and permissions are correct
       lab.succeed("test -e /mnt/pool/data/backup/lab/var/lib/docker-compose-runner/audiobookshelf/config/absdatabase-snapshot-" + labdayofweek + ".sqlite")
       lab.succeed("ls -nd /mnt/pool/data/backup/lab/var/lib/docker-compose-runner/audiobookshelf/config/absdatabase-snapshot-" + labdayofweek + ".sqlite | awk 'NR==1 {if ($3 == 2000) {exit 0} else {exit 1}}'")
-      lab.succeed("test -e /mnt/pool/data/backup/lab/var/lib/docker-compose-runner/audiobookshelf/config/absdatabase.sqlite")
-      lab.succeed("ls -nd /mnt/pool/data/backup/lab/var/lib/docker-compose-runner/audiobookshelf/config/absdatabase.sqlite | awk 'NR==1 {if ($3 == 2000) {exit 0} else {exit 1}}'")
 
     with subtest("General metrics (lab)"):
       print(lab.succeed("cat /etc/hosts"))
