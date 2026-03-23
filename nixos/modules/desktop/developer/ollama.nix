c0d4d6ced41d793404f0ced1b71642ba7683b89c
Andrew Hayzen <ahayzen@gmail.com>
2026-03-23 22:58:10 +0000

    ollama: force context length to around 64k for now
    This is useful for opencode and other tools.

--- nixos/modules/desktop/developer/ollama.nix
+++ nixos/modules/desktop/developer/ollama.nix
@@ -6,12 +6,23 @@
   services.ollama = {
     enable = true;
 
+    environmentVariables = {
+      # Use an increased the context length, useful for opencode
+      # https://docs.ollama.com/context-length
+      # https://docs.ollama.com/integrations/opencode
+      #
+      # NOTE: opencode could configure itself in the future
+      # https://github.com/anomalyco/opencode/issues/3250
+      OLLAMA_CONTEXT_LENGTH = "65536";
+    };
+
     # Allow for containers to be able to connect to ollama
     #
     # NOTE: use the following address from containers
     # docker: `http://host.docker.internal:11434/`
     # podman: `http://host.containers.internal:11434/`
     # Or use --add-host=alias:host-gateway or --network=host
+    # Or use the hostname of the device eg mydevice.local
     host = "0.0.0.0";
 
     # Do not open the firewall for ollama externally
