ae35c5bca1fca7ffc1ecd283e633f2320c5ecc1e
Andrew Hayzen <ahayzen@gmail.com>
2026-04-27 21:27:32 +0100

    pipewire: enable discovery of AirPlay devices

--- nixos/modules/desktop/pipewire.nix
+++ nixos/modules/desktop/pipewire.nix
@@ -12,5 +12,25 @@
     jack.enable = true;
     pulse.enable = true;
     wireplumber.enable = true;
+
+    # Enable AirPlay discovery in pipewire
+    # https://wiki.nixos.org/wiki/PipeWire#AirPlay/RAOP_configuration
+    #
+    # NOTE: opens UDP ports 6001-6002
+    raopOpenFirewall = true;
+    extraConfig.pipewire = {
+      "10-airplay" = {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-raop-discover";
+
+            # increase the buffer size if you get dropouts/glitches
+            # args = {
+            #   "raop.latency.ms" = 500;
+            # };
+          }
+        ];
+      };
+    };
   };
 }
