8c40ca0ff8ff3293948627fabc7fe0a2a89f18cf
Andrew Hayzen <ahayzen@gmail.com>
2026-06-06 20:38:30 +0100

    helix: document options that are enabled
    Signed-off-by: Andrew Hayzen <ahayzen@gmail.com>

--- home-manager/andrew/helix.nix
+++ home-manager/andrew/helix.nix
@@ -31,16 +31,31 @@
           }
         ];
 
+        # Disable checking on save for rust-analyzer
+        #
+        # As this tends to be very slow blocking builds
+        # instead manually run cargo check/clippy/test
         language-server.rust-analyzer.config.checkOnSave = false;
       };
+
       settings = {
         editor = {
+          # Enable colour for the mode indicator
           color-modes = true;
+
+          # Highlight the line of the cursor
           cursorline = true;
+
+          # Force true-color even if the terminal detection failed
+          #
+          # NOTE: this may not be required anymore, was this for running in containers?
           true-color = true;
 
           lsp = {
-            display-messages = true;
+            # Display LSP progress messages below the status line
+            display-progress-messages = true;
+
+            # Display LSP inlay hints
             display-inlay-hints = true;
           };
         };
