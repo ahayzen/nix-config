78f310ef7d3a1fa0a3cfe1070d6f07704d2d522d
Andrew Hayzen <ahayzen@gmail.com>
2026-08-22 11:56:42 +0100

    home-manager: use default config for zellij now has new UI
    Signed-off-by: Andrew Hayzen <ahayzen@gmail.com>

--- home-manager/andrew/zellij.nix
+++ home-manager/andrew/zellij.nix
@@ -3,24 +3,11 @@
 # SPDX-License-Identifier: MPL-2.0
 
 {
-  # zellij uses kdl which is hard to configure in until extraConfig exists
-  # https://github.com/nix-community/home-manager/issues/4659
-  # catppuccin.zellij.enable = true;
+  catppuccin.zellij.enable = true;
 
   programs = {
     zellij = {
       enable = true;
-
-      # zellij uses kdl which is hard to configure in until extraConfig exists
-      # https://github.com/nix-community/home-manager/issues/4659
-      #
-      # settings = {
-      #   # We do not want frames around each pane
-      #   pane_frames = false;
-      #   ui.pane_frames.hide_session_name = true;
-      # };
     };
   };
-
-  xdg.configFile."zellij/config.kdl".source = ./zellij-config.kdl;
 }
