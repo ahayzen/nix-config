99a9287151d8d17a4a3a2885bf900eb1484454f1
Andrew Hayzen <ahayzen@gmail.com>
2026-09-15 20:38:32 +0100

    zellij: hide the session name and startup tips
    Signed-off-by: Andrew Hayzen <ahayzen@gmail.com>

--- home-manager/andrew/zellij.nix
+++ home-manager/andrew/zellij.nix
@@ -8,6 +8,15 @@
   programs = {
     zellij = {
       enable = true;
+
+      settings = {
+        show_startup_tips = false;
+        ui = {
+          pane_frames = {
+            hide_session_name = true;
+          };
+        };
+      };
     };
   };
 }
